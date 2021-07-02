9a409e305209bb302d579caae09d3d5c23959a8f
taylor-tokareff <taylortokareff@gmail.com>
2021-07-01 17:22:03 -0700

    all done

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -2,8 +2,9 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Comment from '../lib/models/Comment.js';
+import UserService from '../lib/services/UserService.js';
 import Gram from '../lib/models/Gram.js';
+import Comment from '../lib/models/Comment.js';
 
 
 
@@ -11,27 +12,21 @@ describe('demo routes', () => {
   let agent;
   let user;
 
-  beforeEach(() => {
-    return setup(pool);
-  });
   beforeEach(async () => {
-    agent = request.agent(app);
-
-    await agent
-      .post('/api/v1/auth/signup')
+    await setup(pool);
+    agent = await request.agent(app);
+    user = await UserService.create({
+      userName: 'Peaches',
+      password: 'password',
+      profilePhotoUrl: 'https://place-puppy.com/300x300'
+    });
+    await agent.post('/api/v1/auth/login')
       .send({
         userName: 'Peaches',
-        password: 'password',
-        profilePhotoUrl: 'https://place-puppy.com/300x300'
-      });
-    user = await agent
-      .post('/api/v1/auth/login')
-      .send({
-        userName: 'Peaches',
-        password: 'password',
-        // profilePhotoUrl: 'https://place-puppy.com/300x300'
+        password: 'password'
       });
   });
+
 
   test('creates a comment via POST', async () => {
     const post = await Gram.insert({
@@ -45,20 +40,38 @@ describe('demo routes', () => {
       .post('/api/v1/comments')
       .send({
         comment: 'thats a good looking biscuit',
-        commentBy: user.userId,
+        commentBy: user.id,
         postId: post.id
       });
 
     expect(res.body).toEqual({
       id: '1',
-      userId: '1',
       comment: 'thats a good looking biscuit',
       commentBy: '1',
       postId: '1'
     });
+  });
 
+  test('deletes comment1', async () => {
+    const post = await Gram.insert({
+      userId: user.id,
+      photoUrl: 'https://place-puppy.com/300x300',
+      caption: 'not actually Peaches',
+      tags: '#imposter'
+    });
 
+    const comment1 = await Comment.insert({
+      comment: 'thats a good looking biscuit',
+      commentBy: user.id,
+      postId: post.id
+    });
 
+    const res = await agent.delete(`/api/v1/comments/${comment1.id}`);
 
+    expect(res.body).toEqual(comment1);
 
   });
+
+
+});
+
